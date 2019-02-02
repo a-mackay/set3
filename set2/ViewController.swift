4afc538f05efc7670694ecd1a61b936e66bbefa5
a-mackay <amk@gegroup.com.au>
2019-02-02 18:06:32 +1100

    Initial Commit

---
+++ set2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  set2
+//
+//  Created by Anthony Mackay on 2/2/19.
+//  Copyright © 2019 Anthony Mackay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
